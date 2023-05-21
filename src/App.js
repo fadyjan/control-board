d7c9150400ed2256c0300aecec00ded46af783b1
fadyjan <fady.jan95@gmail.com>
2023-05-21 04:14:46 +0300

    Create #3 Project Intialization

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { BoardProvider } from "./context/BoardProvider";
+import QuestionsMenu from './components/QuestionsMenu/QuestionsMenu'
+import ChoicesWrapper from './components/ChoicesWrapper/ChoicesWrapper'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BoardProvider>
+
+      <div id="Wrapper">
+        <QuestionsMenu></QuestionsMenu>
+        <ChoicesWrapper></ChoicesWrapper>
+      </div>
+    </BoardProvider>
   );
 }
 
