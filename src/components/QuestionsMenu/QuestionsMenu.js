71afa794f4d2eb6242f6c14dfdf5046f53909d76
fadyjan <fady.jan95@gmail.com>
2023-05-21 11:47:51 +0300

    Update #3 Css Enhancements

--- src/components/QuestionsMenu/QuestionsMenu.js
+++ src/components/QuestionsMenu/QuestionsMenu.js
@@ -1,17 +1,22 @@
-import React, { useContext } from 'react'
-import QuestionCard  from '../QuestionCard/QuestionCard'
-import { BoardContext } from '../../context/BoardContext'
-import modules from './QuestionsMenu.module.css'
+import React, { useContext } from "react";
+import QuestionCard from "../QuestionCard/QuestionCard";
+import { BoardContext } from "../../context/BoardContext";
+import modules from "./QuestionsMenu.module.css";
+import SubmitWrapper from "../SubmitWrapper/SubmitWrapper";
 const QuestionsMenu = () => {
-   const {QuestionsArray} = useContext(BoardContext)
+  const { QuestionsArray } = useContext(BoardContext);
   return (
     <div id={modules.QuestionMenu}>
-      {QuestionsArray.map((element)=>{
-        return <QuestionCard key={element.id} info={element} ></QuestionCard>
-
-      })}
+      <div id={modules.QuestionsCardsWrapper}>
+        {QuestionsArray.map((element) => {
+          return <QuestionCard key={element.id} info={element}></QuestionCard>;
+        })}
+      </div>
+      <div>
+        <SubmitWrapper></SubmitWrapper>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default QuestionsMenu
+export default QuestionsMenu;
